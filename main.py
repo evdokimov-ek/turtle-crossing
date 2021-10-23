058954a72a481322e403e956e1184c1186068116
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-10-23 17:10:22 +0300

    complete game

--- main.py
+++ main.py
@@ -7,8 +7,32 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.bgcolor("black")
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    # Detect collision with the cars
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    if player.is_finished():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
